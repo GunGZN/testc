c5a8b4e1fccc5d8cb0b97a47840280f1729995b4
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-09-30 10:42:27 +0700

    Update menux.sh

--- menux.sh
+++ menux.sh
@@ -1,76 +1,12 @@
 #!/bin/bash
 clear
 clear
-echo -e "***************-- SCRIPT BY LiL Gun-X --********************" | lolcat
-echo -e "\033[0;31m           ____          __     __  ____    _   _ "
-echo -e "\033[0;32m          / ___|  __  __ \ \   / / |  _ \  | \ | |"
-echo -e "\033[0;34m          \___ \  \ \/ /  \ \ / /  | |_) | |  \| |"
-echo -e "\033[0;34m           ___) |  >  <    \ V /   |  __/  | |\  |"
-echo -e "\033[0;35m          |____/  /_/\_\    \_/    |_|     |_| \_|"
-
-echo -e "*****************ติดต่อสอบถามได้ที่ LINE : gzn007 ***************" | lolcat
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-WKT=$(curl -s ipinfo.io/timezone )
-IPVPS=$(curl -s ipinfo.io/ip )
-	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-	freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-	tram=$( free -m | awk 'NR==2 {print $2}' )
-	swap=$( free -m | awk 'NR==4 {print $2}' )
-	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-
-echo -e "* ชนิดซีพียู : $cname     * ความถี่ซีพียู : $freq MHz"
-echo -e "* แกน​ : $cores     * แรม​ : $tram MB"
-echo -e "* ผู้้ให้บริการ : $ISP"     * ระยะเวลาทำงานของระบบ : $up"
-echo -e "* เมือง​ : $CITY     * เวลา​ : $WKT"
-echo -e "* ไอพี​ : $IPVPS"
-echo -e ""
 echo -e "************************************************************" | lolcat
-echo -e "* menu         : สารบัญคำสั่ง"
+echo -e "                           ลิ้งไฟล์ต่างๆ"     
 echo -e "************************************************************" | lolcat
-echo -e "                     SSH AND OPENVPN"
-echo -e "* 1.           : สร้างบัญชี SSH & OpenVPN"
-echo -e "* 2.           : ต่ออายุบัญชี SSH & OpenVPN"
-echo -e "* 3.           : ลบบัญชี SSH & OpenVPN"
-echo -e "* 4.           : แสดงบัญชีผู้ใช้งาน SSH & OpenVPN"
-echo -e "* 5.           : แสดงบัญชี SSH & OpenVPN ทั้งหมด"
-echo -e "* 6.           : ลบบัญชี SSH & OpenVPN ที่หมดอายุแล้ว"
-echo -e "* 7.           : ตั้งค่าการเข้าใช้งานหลายบัญชี"
-echo -e "* 8.           : แสดงบัญชีที่เข้าใช้งานหลายเครื่อง"
-echo -e "* 9.           : รีสตาร์ท  Dropbear, Squid3, OpenVPN และ SSH"
+echo -e "TCP conf ลิงค์​   :  http://$IP:81/TCP.ovpn"
+echo -e "UDP conf ลิงค์​   :  http://$IP:81/UDP.ovpn"
+echo -e "SSL conf ลิงค์​   :  http://$IP:81/SSL.ovpn"
+echo -e "true-nopro conf ลิงค์​   :  http://: 27.254.163.51:81/true-nopro.ovpn"
+echo -e "true-fbgaming ลิงค์​   :  http://$IP:81/true-fbgaming.ovpn"
 echo -e "************************************************************" | lolcat
-echo -e "                     VMESS"
-echo -e "* 10.          : สร้างบัญชี VMESS"
-echo -e "* 11.          : ลบบัญชี VMESS"
-echo -e "* 12.          : ตรวจสอบผู้ใช้งานบัญชี VMESS"             
-echo -e "* 13.          : ต่ออายุบัญชี VMESS"                     
-echo -e "************************************************************" | lolcat
-echo -e "                     TROJAN"
-echo -e "* 14.          : สร้างบัญชี TROJAN"
-echo -e "* 15.          : ลบบัญชี TROJAN"
-echo -e "* 16.          : ตรวจสอบผู้ใช้งานบัญชี TROJAN"             
-echo -e "* 17.          : ต่ออายุบัญชี TROJAN"
-echo -e "************************************************************" | lolcat
-echo -e "                     VLESS"
-echo -e "* 18.          : สร้างบัญชี VLESS"
-echo -e "* 19.          : ลบบัญชี VLESS"
-echo -e "* 20.          : ตรวจสอบผู้ใช้งานบัญชี VLESS"             
-echo -e "* 21.          : ต่ออายุบัญชี VLESS"
-echo -e "************************************************************"  | lolcat
-echo -e "                    ตั้งค่าระบบ"
-echo -e "* backup       : สำรองข้อมูล/รีสโตร์ข้มูล(บัญชีทั้งหมด)" 
-echo -e "* cert         : ต่ออายุ Certificate ของบัญชี V2ray" 
-echo -e "* port         : แก้ไขหรือเปลี่ยนพอร์ต"
-echo -e "* web          : ติดตั้ง​ Webmin"
-echo -e "* reboot       : รีบูตเครื่อง"
-echo -e "* speedtest    : ตรวจสอบความเร็วเซิฟเวอร์"
-echo -e "* info         : แสดงข้อมูลระบบ"
-echo -e "* rootpass     : เปลี่ยนล๊อคอินเป็น ROOT"
-echo -e "* changeport   : เปลี่ยนพ็อตหลักเป็น 80"
-echo -e "* exit         : ออกจากระบบ"
-echo -e "* up           : อัพเดตสคริป"
-echo -e "************************************************************" | lolcat
-echo -e " ขอบคุณมากที่ใช้บริการของเรา"
-echo -e " Copyright ©LiL Gun-X"
-echo -e ""
